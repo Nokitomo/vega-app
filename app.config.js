ce1faf3b1e96c9160ca0b1e8200784acebf6dce2
Nokitomo <aronnegiuseppe@gmail.com>
2026-09-25 20:59:56 +0200

    chore: sync expo app version

--- app.config.js
+++ app.config.js
@@ -103,7 +103,7 @@ module.exports = () => {
       autolinking: {exclude: ['expo-splash-screen']},
       plugins,
       slug: 'vega',
-      version: '3.3.12',
+      version: '3.3.13',
       userInterfaceStyle: 'dark',
       experiments: {
         reactCompiler: true,
@@ -115,7 +115,7 @@ module.exports = () => {
         minSdkVersion: 24,
         edgeToEdgeEnabled: true,
         package: 'com.vega',
-        versionCode: 171,
+        versionCode: 172,
         permissions: [
           'FOREGROUND_SERVICE',
           'FOREGROUND_SERVICE_MEDIA_PLAYBACK',
